d740dfa70ddc846c2fbf8eeff9eb230e76cb449c
Devansh Mahoday <113814933+DeV-21@users.noreply.github.com>
2025-09-14 23:13:57 +0530

    Add files via upload

---
+++ streamlit_llm_demo_deploy.py
@@ -0,0 +1,22 @@
+import os
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+
+st.title("Ask Anything")
+
+with st.sidebar:
+    st.title("Provide your OpenAI API Key")
+    OPENAI_API_KEY = st.text_input("OpenAI API Key", type="password")
+
+if not OPENAI_API_KEY:
+    st.info("Enter your OpenAI API Key to continue.")
+    st.stop()
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=OPENAI_API_KEY)
+
+question = st.text_input("Enter the question:")
+
+if question:
+    response = llm.invoke(question)
+    st.write(response.content)
